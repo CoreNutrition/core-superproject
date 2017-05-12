c7979e9dbc628db69485c67de57de5e6de77b15e
Cyril Tsiboulski <ct@cloudred.com>
2017-05-11 20:46:24 -0400

    Adding product pages templates

--- wp-content/themes/CORE/template-products.php
+++ wp-content/themes/CORE/template-products.php
@@ -10,9 +10,37 @@
 		<div class="slider">
 			<?php include( locate_template( 'templates/content-slideshow.php' ) );	 ?>
 		</div>
+		<div class="row slideshow-overlay">
+			<div class="col-7 col-md-5 offset-md-1">
+				<?php the_content(); ?>
+			</div>
+			<div class="col-5 col-md-4 offset-md-1 img-overlay">
+			<?php $lifestyle_image_overlay = get_field('lifestyle_image_overlay');
+				if ($lifestyle_image_overlay) {
+					echo "<img src='".$lifestyle_image_overlay['sizes']['medium']."' alt='".$lifestyle_image_overlay['alt']."' />";
+				}
+			?>
+			</div>
+		</div>
 	</section>
 	
+	<section class="facts" style="<?php echo get_field('facts_gradient_css'); ?>">
+		<?php if (get_field('graphic_display_mode') == "above") {
+					$fact_graphic = get_field('fact_graphic');
+					$fact = get_field('fact');
+					if ($fact_graphic) {
+						echo "<img src='".$fact_graphic['sizes']['large']."' alt='".$fact_graphic['alt']."' />";
+					}
+					if ($fact) {
+						echo "<div class='content-horizontal'>";
+							echo $fact;
+						echo "</div>";
+					}
+				}
+		?>
+
 	
+	</section>
 	
 	<section class="lifestyle">
 	<?php
@@ -23,15 +51,20 @@
 		if( $lifestyle_photos ):
 			echo "<div class='featured-posts grid'>";
 				foreach( $lifestyle_photos as $lifestyle_photo ): 
-        			echo "<div class='grid-item'>";
+        			echo "<div class='grid-item half'>";
         				echo "<img src='".$lifestyle_photo['sizes']['medium']."' alt='".$lifestyle_photo['alt']."' />";
        				echo "</div>";
+       				$counter ++;
+       				if ( $counter == 2 ) {
+       					echo "<div class='grid-item half content-box-offset'>";
+        					echo "<h2>".$lifestyle_callout_text."</h2>";
+       					echo "</div>";
+       				}
             	endforeach;
             	echo "<div class='grid-sizer'></div>";
             	echo "<div class='gutter-sizer'></div>";
             echo "</div>";
         endif;
-        //echo $lifestyle_callout_text; 
     ?>  
 	</section>
 	
--- wp-content/themes/CORE/templates/content-slideshow.php
+++ wp-content/themes/CORE/templates/content-slideshow.php
@@ -17,15 +17,20 @@ if( have_rows('featured_slideshow') ):
 		?>
 		<div class="item" style="background-image:url('<?php echo $image['url']; ?>');">
             <!--<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />-->
+            <?php if ($title) { ?>
             <div class="container overlay-content">
               <div class="row">
                   <h1 class="feature <?php echo $color_scheme; ?>"> <?php echo $title; ?></h1>
+                  <?php if ($button_text && $button_link) { ?>
                   <a href="<?php echo $button_link; ?>" class="white-btn"><?php echo $button_text; ?></a>
+                  <?php } ?>
               </div>
               <?php if ($video_embed) { ?>
               	<a href="#" data-toggle="modal" data-target="#modal" class="play hidden-xs"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/play-button.svg" alt="<?php _e("Play video","sage"); ?>"></a>
               <?php } ?>
             </div>
+            <?php } ?>
+            
           </div>
 		<?php
     endwhile;
