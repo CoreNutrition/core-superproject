fb463433f497760f5dfe1a7a4dbaeebee6c8a05b
Cyril Tsiboulski <ct@cloudred.com>
2017-05-17 14:02:02 -0400

    Added an option to set a custom color for the page headers

--- wp-content/themes/CORE/templates/page-header.php
+++ wp-content/themes/CORE/templates/page-header.php
@@ -3,7 +3,13 @@
 	<div class="row">
 		<div class="col-12">
 			<div class="page-header">
-			  <h1><?= Titles\title(); ?></h1>
+			<?php
+			  if (get_field('header_color')) {
+			  	$header_style = " style='color:".get_field('header_color')."'";
+			  } else {
+			  	$header_style="";
+			  } ?>
+			  <h1<?php echo $header_style; ?>><?= Titles\title(); ?></h1>
 			</div>
 		</div>
 	</div>
