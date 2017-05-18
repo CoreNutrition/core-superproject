6dbe91415f90276a6005bcabe0af90fd30f8c1b6
Cyril Tsiboulski <ct@cloudred.com>
2017-05-18 17:37:10 -0400

    Updated nutritional label pos for larger screens, changed background positioning for product listing header images

--- wp-content/themes/CORE/templates/content-item-nutrition.php
+++ wp-content/themes/CORE/templates/content-item-nutrition.php
@@ -13,13 +13,13 @@
 	<!-- Modal -->
 	<div class="modal-label animated fadeIn lity-hide container" id="<?php echo str_replace(' ', '', $product_name); ?>" tabindex="-1" style="overflow:auto; background: <?php echo $product_color; ?>">
 	 	<div class="row" style="padding: 2rem 1.333rem;">
-			<div class="col-md-6">
+			<div class="col-lg-6">
 				<div>
 					<h2><?php echo $product_name; ?></h2>
 					<?php echo $nutritional_info; ?>
 				</div>
 		    </div>
-		    <div class="col-md-6">
+		    <div class="col-lg-6">
 		    <img src="<?php echo $nutrition_facts_label['sizes']['medium']; ?>" alt="<?php echo $nutrition_facts_label['alt']; ?>" />
 		    </div>
 		</div>
